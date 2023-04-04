eb51df6bd93305fcc8f884a1a238f5145b4d805c
evgenydmorozov <evgenydmorozov@gmail.com>
2023-04-04 16:39:26 +0500

    chore(lang): add location in English lang file

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -4,5 +4,22 @@ return [
     'plugin' => [
         'name' => 'Locations',
         'description' => 'Easy to use system for managing company locations',
-    ]
+    ],
+    'location' => [
+        'id' => 'ID',
+        'name' => 'Name',
+        'description' => 'Description',
+        'created_at' => 'Created at',
+        'updated_at' => 'Updated at',
+        'country' => 'Country',
+        'region' => 'Region',
+        'locality' => 'Locality',
+        'street' => 'Street',
+        'geo_latitude' => 'Geocode (latitude)',
+        'geo_longitude' => 'Geocode (longitude)',
+        'postal_code' => 'Postal code',
+        'fax' => 'Fax',
+        'post_office_box' => 'Post office box',
+        'opening_hours' => 'Opening hours',
+    ],
 ];
