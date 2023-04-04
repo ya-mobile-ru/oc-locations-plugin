ba6c126b3770a606e886aa394bb7f359e068f14f
evgenydmorozov <evgenydmorozov@gmail.com>
2023-04-04 16:39:48 +0500

    chore(lang): add location in Russian lang file

--- lang/ru/lang.php
+++ lang/ru/lang.php
@@ -4,5 +4,22 @@ return [
     'plugin' => [
         'name' => 'Локации',
         'description' => 'Простая система для управления локациями компании',
-    ]
+    ],
+    'location' => [
+        'id' => 'ID',
+        'name' => 'Название',
+        'description' => 'Описание',
+        'created_at' => 'Создано',
+        'updated_at' => 'Обновлено',
+        'country' => 'Страна',
+        'region' => 'Регион',
+        'locality' => 'Населённый пункт',
+        'street' => 'Улица',
+        'geo_latitude' => 'Geocode (широта)',
+        'geo_longitude' => 'Geocode (долгота)',
+        'postal_code' => 'Индекс',
+        'fax' => 'Факс',
+        'post_office_box' => 'Абонентский ящик',
+        'opening_hours' => 'Часы работы',
+    ],
 ];
